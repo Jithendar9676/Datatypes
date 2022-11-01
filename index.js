290221ba35aaaa32f9c7d081941cdf4d55a9b09c
Jithendar reddy <94469047+Jithendar9676@users.noreply.github.com>
2022-11-01 08:57:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// n ss bb ss u
+let a=null;
+let b=345;
+let c=true;// false
+let d=BigInt("56")
+let e="Jithendar"
+let f=Symbol("I am a symbol")
+let g
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+
+
+//object 
+const item={
+  "harry":true
+}
+console.log(item["harry"])
